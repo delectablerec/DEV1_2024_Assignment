35344c1d2795b5e8fd8052b739f800c50c93ea01
r4ni4st4r <francesco.basevi@gmail.com>
2024-11-11 15:44:45 +0100

    archetype changed

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DEV1_2024_Assignment.Models;
+
+namespace DEV1_2024_Assignment.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-"# DEV1_2024_Assignment" 
-"# DEV1_2024_Assignment" 
-"# DEV1_2024_Assignment_2" 
-"# DEV1_2024_Assignment_2" 
-"# DEV1_2024_Assignment_2" 
