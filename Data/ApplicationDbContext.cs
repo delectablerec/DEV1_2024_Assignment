9fa0074b4d77628776becf185e9bc05f071b9017
r4ni4st4r <francesco.basevi@gmail.com>
2024-11-11 14:55:19 +0100

    archetype created

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DEV1_2024_Assignment.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
