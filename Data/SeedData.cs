b80064ba92edd05845f0f7b6c609cfd93bab7f53
mtm90 <mattia22nchi@gmail.com>
2024-11-12 14:04:52 +0100

    seedData fix

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -5,16 +5,19 @@ namespace DEV1_2024_Assignment.Data
 {
     public class SeedData
     {
-       public static async Task InitializeAsync(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager){
-            string[] roleNames = {"Admin", "Customer", "Brand"};
-            foreach(var roleName in roleNames)
+        public static async Task InitializeAsync(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string[] roleNames = { "Admin", "Customer", "Brand" };
+            foreach (var roleName in roleNames)
             {
-                if(!await roleManager.RoleExistsAsync(roleName))
-                await roleManager.CreateAsync(new IdentityRole(roleName));
+                if (!await roleManager.RoleExistsAsync(roleName))
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
             }
-    
-            if(await userManager.FindByEmailAsync("admin@admin.com") == null){
-                var adminUser = new AppUser{
+
+            if (await userManager.FindByEmailAsync("admin@admin.com") == null)
+            {
+                var adminUser = new AppUser
+                {
                     Name = "Admin",
                     Surname = "AdminSur",
                     Address = "pippo",
@@ -23,15 +26,20 @@ namespace DEV1_2024_Assignment.Data
                     EmailConfirmed = true
                 };
                 var result = await userManager.CreateAsync(adminUser, "Password1@");
-                if(result.Succeeded){
+                if (result.Succeeded)
+                {
                     await userManager.AddToRoleAsync(adminUser, "Admin");
-                    await userManager.AddToRoleAsync(adminUser, "Admin");
-                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                    await userManager.AddToRoleAsync(adminUser, "Customer");
+                    await userManager.AddToRoleAsync(adminUser, "Brand");
                 }
-            }else{
+            }
+            else
+            {
                 var adminUser = await userManager.FindByEmailAsync("admin@admin.com");
                 await userManager.AddToRoleAsync(adminUser, "Admin");
+                await userManager.AddToRoleAsync(adminUser, "Customer");
+                await userManager.AddToRoleAsync(adminUser, "Brand");
             }
-       }
+        }
     }
 }
