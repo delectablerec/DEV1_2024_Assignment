2c8e3a4fc6b8469753ebfc2451b5950afb8094a3
r4ni4st4r <francesco.basevi@gmail.com>
2024-11-12 17:49:30 +0100

    last update

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,27 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using DEV1_2024_Assignment.ViewModels;
 using DEV1_2024_Assignment.Services;
-using DEV1_2024_Assignment.Models;
-
 
 namespace DEV1_2024_Assignment.Controllers;
 
-    public class ProductsController : Controller
-    {
-        private ServiceProducts _service;
-
-        public ProductsController(ServiceProducts service){
-            _service = service;
-        }
-
-
-        [HttpGet]
-        public IActionResult Details(int id)
-        {
-            var model = new DetailsViewModel();       
-            model.Product = _service.GetProductById(id);
-            return View(model);
-        }
-
+public class ProductsController : Controller
+{
+    private ServiceProducts _service;
+    public ProductsController(ServiceProducts service){
+        _service = service;
     }
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+        var model = new DetailsViewModel();       
+        model.Product = _service.GetProductById(id);
+        return View(model);
+    }
+}
 
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,7 +15,7 @@ public class ApplicationDbContext : IdentityDbContext<AppUser>
     private DbSet<Purchase> _purchases { get; set; }
 
     public List<Product> GetProducts(){
-        return new List<Product>();
+        return _products.ToList();
     }
     public void AddProduct(){
         
