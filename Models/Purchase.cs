123e478176356932cb17d8f5ee186c940d20ab8d
r4ni4st4r <francesco.basevi@gmail.com>
2024-11-12 09:43:46 +0100

    little changes

--- Models/Purchase.cs
+++ Models/Purchase.cs
@@ -3,7 +3,7 @@ public class Purchase
 {
     public int Id {get;set;}
     public int CustomerId {get;set;}
-    public Customer Customer {get;set;}
+    public AppUser Customer {get;set;}
     public int ProductId {get;set;}   
     public Product Product {get;set;} 
     public int Quantity {get;set;}  
