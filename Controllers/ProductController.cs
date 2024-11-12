0db5176a90bba3d5509052d133dfb7d19cf8f7de
mtm90 <mattia22nchi@gmail.com>
2024-11-12 15:05:49 +0100

    added controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using DEV1_2024_Assignment.Data;
+using DEV1_2024_Assignment.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace DEV1_2024_Assignment.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Product product)
+        {
+            if (!ModelState.IsValid) return View(product);
+
+            _context.Add(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
