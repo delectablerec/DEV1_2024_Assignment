69c32a29051c242cb72af98ac6c34ac6dcebba7f
r4ni4st4r <francesco.basevi@gmail.com>
2024-11-12 15:32:05 +0100

    viewModelFixed

---
+++ Services/ServiceProducts.cs
@@ -0,0 +1,27 @@
+using DEV1_2024_Assignment.Models;
+using DEV1_2024_Assignment.Data;
+
+namespace DEV1_2024_Assignment.Services
+{
+    
+    public class ServiceProducts
+    {
+        private ApplicationDbContext _database;
+        public List<Product> GetProducts(){
+            return _database.GetProducts();
+        }
+        public Product GetProductById(int id){
+            Product foundProduct = null;
+            // Loop through each product to find the one with the matching ID
+            foreach (var product in _database.GetProducts())
+            {
+                if (product.Id == id)
+                {
+                    foundProduct = product;
+                    break;
+                }
+            }
+            return foundProduct;
+        }
+    }
+}
