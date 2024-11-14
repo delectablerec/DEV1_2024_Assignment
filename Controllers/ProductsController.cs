33aac0949cb9895896c203fc6cf2e3bda948cd70
r4ni4st4r <francesco.basevi@gmail.com>
2024-11-14 17:44:42 +0100

    restart pro

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -86,5 +86,4 @@ public class ProductsController : Controller
         
         return View(model);
     }
-    
 }
--- ViewModels/CartViewModel.cs
+++ ViewModels/CartViewModel.cs
@@ -4,5 +4,4 @@ namespace DEV1_2024_Assignment.ViewModels;
 public class CartViewModel
 {
     public AppUser Customer { get; set; }
-
 }
